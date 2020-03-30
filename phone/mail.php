13f9cfbbd66d76e6519235afdda5e7e7cd7d17cc
maxim-denisov <57037291+maxim-denisov@users.noreply.github.com>
2020-03-30 17:35:43 +0300

    jgj

---
+++ phone/mail.php
@@ -0,0 +1,11 @@
+<?php
+$to = "maximdenisovofficial@gmail.com"; // емайл получателя данных из формы
+$tema = "Форма обратной связи на PHP"; // тема полученного емайла
+$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
+$message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
+$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
+$message .= "Сообщение: ".$_POST['message']."<br>"; //полученное из формы name=message
+$headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+?>
