e824221d952168b4dec1c89ce09552642bf0d488
maxim-denisov <57037291+maxim-denisov@users.noreply.github.com>
2022-03-16 19:46:33 +0200

    1

--- cryptoexchange/js/script.js
+++ cryptoexchange/js/script.js
@@ -6,3 +6,16 @@ $(document).ready(function () {
     });
   });
 });
+
+
+$(document).ready(function () {
+  $("#myInput1").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#search-text1 .coin-bg").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+});
+
+
+
