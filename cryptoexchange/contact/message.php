ca53c576dc92cdf811b0780892d3dadab9f11c78
maxim-denisov <57037291+maxim-denisov@users.noreply.github.com>
2022-03-19 20:23:05 +0200

    1

--- cryptoexchange/contact/message.php
+++ cryptoexchange/contact/message.php
@@ -6,7 +6,7 @@
   $message = htmlspecialchars($_POST['message']);
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "receiver_email_address"; //enter that email address where you want to receive all messages
+      $receiver = "nrnaafn@cutradition.com"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
       $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
