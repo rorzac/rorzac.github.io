1eb7a7584b4ffe87bd3bf97f51259b729f371d3e
rorzac <rrorzac@gmail.com>
2022-06-02 14:54:32 +0300

    1

--- exmocoinnet/script/pricecoin.js
+++ exmocoinnet/script/pricecoin.js
@@ -1,7 +1,69 @@
 let btc_usd = 29942.62;
-let btc_uah = 29942.62;
-let btc_rub = 29942.62;
-let btc_eur = 29942.62;
+let btc_uah = 883896.16;
+let btc_rub = 1840758.59;
+let btc_eur = 27923.98;
 
+let eth_usd = 1823.13;
+let eth_uah = 53893.23;
+let eth_rub = 112345.19;
+let eth_eur = 1703.44;
 
+let dash_usd = 
+let dash_uah =
+let dash_rub =
+let dash_eur =   
 
+let trx_usd = 
+let trx_uah =
+let trx_rub =
+let trx_eur = 
+
+let xrp_usd = 
+let xrp_uah =
+let xrp_rub =
+let xrp_eur = 
+
+let zcash_usd = 
+let zcash_uah =
+let zcash_rub =
+let zcash_eur = 
+
+let ada_usd = 
+let ada_uah =
+let ada_rub =
+let ada_eur = 
+
+let ltc_usd = 
+let ltc_uah =
+let ltc_rub =
+let ltc_eur = 
+
+let bnb_usd = 
+let bnb_uah =
+let bnb_rub =
+let bnb_eur = 
+
+let polygon_usd = 
+let polygon_uah =
+let polygon_rub =
+let polygon_eur = 
+
+let sol_usd = 
+let sol_uah =
+let sol_rub =
+let sol_eur = 
+
+let sol_usd = 
+let sol_uah =
+let sol_rub =
+let sol_eur = 
+
+let atom_usd = 
+let atom_uah =
+let atom_rub =
+let atom_eur = 
+
+let usdt_usd = 1;
+let usdt_uah = 36;
+let usdt_rub = 68;
+let usdt_eur = 0.8;
