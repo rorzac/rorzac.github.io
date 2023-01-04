65b9cc6c4eeeefaaef84253d1a5be951aca3838a
rorzac <rrorzac@gmail.com>
2023-01-04 15:23:23 +0200

    1

---
+++ templatemo/telegram.php
@@ -0,0 +1,32 @@
+<?php
+
+/* https://api.telegram.org/bot5892191579:AAFVoS2cwhyXM92S4tq5ly4eEixnZv0ksj4/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$telegram = $_POST['telegram'];
+$email = $_POST['email'];
+$country = $_POST['country'];
+$message = $_POST['message'];
+$token = "5892191579:AAFVoS2cwhyXM92S4tq5ly4eEixnZv0ksj4";
+$chat_id = "-847715415";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телеграм: ' => $telegram,
+  'Email:' => $email,
+  'Страна:' => $country,
+  'Его сообщение:' => $message
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: index.html');
+} else {
+  echo "Ошибка";
+}
+?>
