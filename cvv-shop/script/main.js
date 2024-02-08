d1c5569b47725bc81e4c0da1dadc9a5e3a41c56e
rorzac <62541393+rorzac@users.noreply.github.com>
2024-02-08 17:43:59 +0200

    1

--- cvv-shop/script/main.js
+++ cvv-shop/script/main.js
@@ -1,25 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var sequence = ""; // Variable to store the entered sequence
-
-    // Event listener for clicking on numbers
-    document.querySelectorAll('.number').forEach(function(number) {
-        number.addEventListener('click', function() {
-            sequence += this.querySelector('span').innerText; // Append clicked number to the sequence
-            checkSequence(); // Check if the sequence matches the desired one
-        });
-    });
-
-    // Function to check if the entered sequence matches the desired one
-    function checkSequence() {
-        var targetSequence = "2393";
-        var sequenceLength = sequence.length;
-        var targetSequenceLength = targetSequence.length;
-
-        if (sequenceLength >= targetSequenceLength) {
-            var lastDigits = sequence.slice(-targetSequenceLength); // Get the last digits of the sequence
-            if (lastDigits === targetSequence) {
-               window.location.href = "/cvv-shop/login.html";
-            }
-        }
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var password = document.getElementById('passwordInput').value;
+    if (password === '2393') {
+        window.location.href = '/login.html';
+    } else {
+        document.getElementById('passwordInput').value = ''; // Очищаем поле ввода
+        document.getElementById('message').textContent = 'Пароль неверный. Попробуйте еще раз.';
     }
 });
