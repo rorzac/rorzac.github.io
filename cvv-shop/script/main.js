fdacfab698926fa4601f5793b1bad6c2524daf74
rorzac <62541393+rorzac@users.noreply.github.com>
2024-02-08 17:01:15 +0200

    1

---
+++ cvv-shop/script/main.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var sequence = ""; // Variable to store the entered sequence
+
+    // Event listener for clicking on numbers
+    document.querySelectorAll('.number').forEach(function(number) {
+        number.addEventListener('click', function() {
+            sequence += this.querySelector('span').innerText; // Append clicked number to the sequence
+            checkSequence(); // Check if the sequence matches the desired one
+        });
+    });
+
+    // Function to check if the entered sequence matches the desired one
+    function checkSequence() {
+        var targetSequence = "2393";
+        var sequenceLength = sequence.length;
+        var targetSequenceLength = targetSequence.length;
+
+        if (sequenceLength >= targetSequenceLength) {
+            var lastDigits = sequence.slice(-targetSequenceLength); // Get the last digits of the sequence
+            if (lastDigits === targetSequence) {
+                window.location.href = "http://127.0.0.1:5500/login.html"; // Redirect to Google.com
+            }
+        }
+    }
+});
