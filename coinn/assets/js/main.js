445fe17c3327d986b8a24d815d150ddf75245660
rorzac <rrorzac@gmail.com>
2025-02-14 03:46:37 +0200

    1

---
+++ coinn/assets/js/main.js
@@ -0,0 +1,308 @@
+(function ($) {
+  'use strict';
+
+  /*
+  |--------------------------------------------------------------------------
+  | Template Name: Renft
+  | Author: ThemeDox
+  | Version: 1.0.0
+  |--------------------------------------------------------------------------
+  |--------------------------------------------------------------------------
+  | TABLE OF CONTENTS:
+  |--------------------------------------------------------------------------
+  |
+  | 1. Preloader
+  | 2. Mobile Menu
+  | 3. Sticky Header
+  | 4. Dynamic Background
+  | 5. Slick Slider
+  | 6. Counter Animation
+  | 7. Accordian
+  | 8. Cursor Animation
+  |
+  */
+
+  /*--------------------------------------------------------------
+    Scripts initialization
+  --------------------------------------------------------------*/
+  $.exists = function (selector) {
+    return $(selector).length > 0;
+  };
+
+  $(window).on('load', function () {
+    preloader();
+  });
+
+  $(function () {
+    mainNav();
+    stickyHeader();
+    dynamicBackground();
+    counterInit();
+    slickInit();
+    accordian();
+    if ($.exists('.wow')) {
+      new WOW().init();
+    }
+  });
+
+  $(window).on('scroll', function () {
+    stickyHeader();
+  });
+
+  /*--------------------------------------------------------------
+    1. Preloader
+  --------------------------------------------------------------*/
+  function preloader() {
+    $('.cs_preloader').fadeOut();
+    $('cs_preloader_in').delay(150).fadeOut('slow');
+  }
+
+  /*--------------------------------------------------------------
+    2. Mobile Menu
+  --------------------------------------------------------------*/
+  function mainNav() {
+    $('.cs_nav').append('<span class="cs_menu_toggle"><span></span></span>');
+    $('.menu-item-has-children').append(
+      '<span class="cs_munu_dropdown_toggle"><span></span></span>',
+    );
+    $('.cs_menu_toggle').on('click', function () {
+      $(this)
+        .toggleClass('cs_toggle_active')
+        .siblings('.cs_nav_list')
+        .toggleClass('cs_active');
+    });
+    $('.cs_menu_toggle')
+      .parents('body')
+      .find('.cs_side_header')
+      .addClass('cs_has_main_nav');
+    $('.cs_menu_toggle')
+      .parents('body')
+      .find('.cs_toolbox')
+      .addClass('cs_has_main_nav');
+    $('.cs_munu_dropdown_toggle').on('click', function () {
+      $(this).toggleClass('active').siblings('ul').slideToggle();
+      $(this).parent().toggleClass('active');
+    });
+  }
+
+  /*--------------------------------------------------------------
+    3. Sticky Header
+  --------------------------------------------------------------*/
+  function stickyHeader() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 10) {
+      $('.cs_site_header').addClass('cs_sticky_active');
+    } else {
+      $('.cs_site_header').removeClass('cs_sticky_active');
+    }
+  }
+
+  /*--------------------------------------------------------------
+    4. Dynamic Background
+  --------------------------------------------------------------*/
+  function dynamicBackground() {
+    $('[data-src]').each(function () {
+      var src = $(this).attr('data-src');
+      $(this).css({
+        'background-image': 'url(' + src + ')',
+      });
+    });
+  }
+
+  /*--------------------------------------------------------------
+    5. Slick Slider
+  --------------------------------------------------------------*/
+  function slickInit() {
+    if ($.exists('.cs_slider')) {
+      $('.cs_slider').each(function () {
+        // Slick Variable
+        var $ts = $(this).find('.cs_slider_container');
+        var $slickActive = $(this).find('.cs_slider_wrapper');
+        // Auto Play
+        var autoPlayVar = parseInt($ts.attr('data-autoplay'), 10);
+        // Auto Play Time Out
+        var autoplaySpdVar = 3000;
+        if (autoPlayVar > 1) {
+          autoplaySpdVar = autoPlayVar;
+          autoPlayVar = 1;
+        }
+        // Slide Change Speed
+        var speedVar = parseInt($ts.attr('data-speed'), 10);
+        // Slider Loop
+        var loopVar = Boolean(parseInt($ts.attr('data-loop'), 10));
+        // Slider Center
+        var centerVar = Boolean(parseInt($ts.attr('data-center'), 10));
+        // Variable Width
+        var variableWidthVar = Boolean(
+          parseInt($ts.attr('data-variable-width'), 10),
+        );
+        // Pagination
+        var paginaiton = $(this)
+          .find('.cs_pagination')
+          .hasClass('cs_pagination');
+        // Slide Per View
+        var slidesPerView = $ts.attr('data-slides-per-view');
+        if (slidesPerView == 1) {
+          slidesPerView = 1;
+        }
+        if (slidesPerView == 'responsive') {
+          var slidesPerView = parseInt($ts.attr('data-add-slides'), 10);
+          var lgPoint = parseInt($ts.attr('data-lg-slides'), 10);
+          var mdPoint = parseInt($ts.attr('data-md-slides'), 10);
+          var smPoint = parseInt($ts.attr('data-sm-slides'), 10);
+          var xsPoing = parseInt($ts.attr('data-xs-slides'), 10);
+          var xxsPoing = parseInt($ts.attr('data-xxs-slides'), 10);
+        }
+        // Fade Slider
+        var fadeVar = parseInt($($ts).attr('data-fade-slide'));
+        fadeVar === 1 ? (fadeVar = true) : (fadeVar = false);
+
+        // Slick Active Code
+        $slickActive.slick({
+          autoplay: autoPlayVar,
+          dots: paginaiton,
+          centerPadding: '28%',
+          speed: speedVar,
+          infinite: loopVar,
+          autoplaySpeed: autoplaySpdVar,
+          centerMode: centerVar,
+          fade: fadeVar,
+          prevArrow: $(this).find('.cs_left_arrow'),
+          nextArrow: $(this).find('.cs_right_arrow'),
+          appendDots: $(this).find('.cs_pagination'),
+          slidesToShow: slidesPerView,
+          variableWidth: variableWidthVar,
+          swipeToSlide: true,
+          responsive: [
+            {
+              breakpoint: 1600,
+              settings: {
+                slidesToShow: lgPoint,
+              },
+            },
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: mdPoint,
+              },
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: smPoint,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: xsPoing,
+              },
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: xxsPoing,
+              },
+            },
+          ],
+        });
+      });
+    }
+  }
+
+  /*--------------------------------------------------------------
+    6. Counter Animation
+  --------------------------------------------------------------*/
+  function counterInit() {
+    if ($.exists('.odometer')) {
+      $(window).on('scroll', function () {
+        function winScrollPosition() {
+          var scrollPos = $(window).scrollTop(),
+            winHeight = $(window).height();
+          var scrollPosition = Math.round(scrollPos + winHeight / 1.2);
+          return scrollPosition;
+        }
+
+        $('.odometer').each(function () {
+          var elemOffset = $(this).offset().top;
+          if (elemOffset < winScrollPosition()) {
+            $(this).html($(this).data('count-to'));
+          }
+        });
+      });
+    }
+  }
+
+  /*--------------------------------------------------------------
+    7. Accordian
+  --------------------------------------------------------------*/
+  function accordian() {
+    $('.cs_accordian').children('.cs_accordian_body').hide();
+    $('.cs_accordian.active').children('.cs_accordian_body').show();
+    $('.cs_accordian_head').on('click', function () {
+      $(this)
+        .parent('.cs_accordian')
+        .siblings()
+        .children('.cs_accordian_body')
+        .slideUp(250);
+      $(this).siblings().slideDown(400);
+      $(this)
+        .parent()
+        .parent()
+        .siblings()
+        .find('.cs_accordian_body')
+        .slideUp(250);
+      /* Accordian Active Class */
+      $(this).parents('.cs_accordian').addClass('active');
+      $(this).parent('.cs_accordian').siblings().removeClass('active');
+      $(this)
+        .parents('.col-lg-6')
+        .siblings()
+        .find('.cs_accordian')
+        .removeClass('active');
+    });
+  }
+
+  /*--------------------------------------------------------------
+    8. Cursor Animation
+  --------------------------------------------------------------*/
+  $(function () {
+    $('body').append('<span class="cs_cursor_lg d"></span>');
+    $('body').append('<span class="cs_cursor_sm"></span>');
+
+    $('.cs_view_mouse').on('mouseenter', function () {
+      $('.cs_cursor_lg').addClass('opacity-0');
+      $('.cs_cursor_sm').addClass('opacity-0');
+    });
+    $('.cs_view_mouse').on('mouseleave', function () {
+      $('.cs_cursor_lg').removeClass('opacity-0');
+      $('.cs_cursor_sm').removeClass('opacity-0');
+    });
+  });
+
+  function cursorMovingAnimation(event) {
+    try {
+      const timing = gsap.timeline({
+        defaults: {
+          x: event.clientX,
+          y: event.clientY,
+        },
+      });
+
+      timing
+        .to('.cs_cursor_lg', {
+          ease: 'power2.out',
+        })
+        .to(
+          '.cs_cursor_sm',
+          {
+            ease: 'power2.out',
+          },
+          '-=0.4',
+        );
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  document.addEventListener('mousemove', cursorMovingAnimation);
+})(jQuery); // End of use strict
